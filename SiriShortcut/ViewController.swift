fbb7af497dc8efb165f0aa10c678cf2fde32f9ba
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2018-10-30 19:27:35 -0400

    Initial Commit

---
+++ SiriShortcut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcut
+//
+//  Created by Ryan Cobelli on 10/30/18.
+//  Copyright © 2018 rybel-llc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
